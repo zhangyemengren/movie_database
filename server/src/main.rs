e40a10f872cec60a05ce60aaf8df8294b958f834
zhangyemengren <624593758@qq.com>
2024-07-07 08:57:38 +0800

    update

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,6 +1,20 @@
 use axum::{routing::get, Router};
+use reqwest;
+use reqwest::header::HeaderMap;
 
 async fn root() -> &'static str {
+    let client = reqwest::Client::new();
+    let key = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZTRjMmUzNzIwY2EzNjc5ZTdkMzZlYTlmMmU0YjY3ZCIsIm5iZiI6MTcyMDI2NDU3Ny43MTY1OSwic3ViIjoiNjU0Y2Q1ODMxYWMyOTI3YjJkY2ZmYzZjIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.g61yEF8VbD8zgFbnS28nXRSmbrHm9lE-yB2z-mceKJQ";
+    let h1 = format!("Bearer {}", key);
+    let mut headers = HeaderMap::new();
+    headers.insert("Authorization", h1.parse().unwrap());
+    let res = client
+        .get("https://api.themoviedb.org/3/trending/movie/week?language=zh-CN")
+        .headers(headers)
+        .send()
+        .await
+        .unwrap();
+    println!("{:?}", res.text().await.unwrap());
     "Hello, World!"
 }
 
@@ -8,6 +22,6 @@ async fn root() -> &'static str {
 async fn main() {
     let app = Router::new().route("/", get(root));
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }

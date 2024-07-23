e0327aef37e956587810529462e97ba6a243c834
zhangyemengren <624593758@qq.com>
2024-07-23 11:40:55 +0800

    update

--- client/src/pages/index.rs
+++ client/src/pages/index.rs
@@ -1,12 +1,13 @@
 use leptos::{component, IntoView, view};
 use log::debug;
-use web_sys;
+use web_sys::{self, js_sys};
+
 
 pub fn t(){
     debug!("Hello, Worl!d!");
-    debug!("{:?}", web_sys::window().unwrap().location().href());
-    web_sys::window().unwrap().document().unwrap().start_view_transition().unwrap();
-    // start_view_transition();
+    let func = js_sys::Function::new_no_args("window.location.href = '/movie/1';");
+    let x = web_sys::window().unwrap().document().unwrap().start_view_transition_with_update_callback(Some(&func)).unwrap();
+    debug!("{:?}", x);
     // let navigate = leptos_router::use_navigate();
     // navigate("/movie/1", Default::default());
 }
@@ -17,7 +18,9 @@ pub fn Index() -> impl IntoView{
     view! {
         <div class="h-full">
             <h1>"Hello, World!"</h1>
-            <div class="flex justify-center items-center bg-[red] w-[200px] h-[200px]">box</div>
+            <div
+                class="flex justify-center items-center bg-[red] w-[200px] h-[200px] box"
+            >box</div>
             <button on:click=move |_| {t()}>click me</button>
         </div>
     }
--- client/src/pages/movie_detail.rs
+++ client/src/pages/movie_detail.rs
@@ -5,7 +5,9 @@ pub fn MovieDetail() -> impl IntoView{
     view! {
         <div class="h-full">
             <h1>Movie Detail</h1>
-            <div class="flex justify-center items-center bg-[red] w-[200px] h-[200px] rotate-45 scale-50">box</div>
+            <div
+                class="flex justify-center items-center bg-[red] w-[200px] h-[200px] scale-50 rotate-45 box"
+            >box</div>
             <p>123</p>
         </div>
     }
